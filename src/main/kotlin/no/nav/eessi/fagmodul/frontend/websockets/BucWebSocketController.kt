224d1db05a5f8f65eb09d9e0a1ba9262feab6565
Nuno Cardoso <Nuno.Cardoso@nav.no>
2019-07-26 15:24:18 +0100

    x

--- src/main/kotlin/no/nav/eessi/fagmodul/frontend/websockets/BucWebSocketController.kt
+++ src/main/kotlin/no/nav/eessi/fagmodul/frontend/websockets/BucWebSocketController.kt
@@ -2,6 +2,7 @@ package no.nav.eessi.fagmodul.frontend.websockets
 
 import no.nav.eessi.fagmodul.frontend.utils.mapAnyToJson
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Description
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.simp.SimpMessagingTemplate
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller
 
 @EnableScheduling
 @Controller
+@Description("This controller's purpose is to verify the websocket status during development, and should be delete when development is finished")
 class BucWebSocketController {
 
     @Autowired
