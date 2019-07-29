bdfa2c71af9d33dc122bf6b1507458cfc85fb3d7
Nuno Cardoso <Nuno.Cardoso@nav.no>
2019-07-29 07:08:06 +0100

    Loggingpoint when WebSocketConfigurer endpoint is created

--- src/main/kotlin/no/nav/eessi/fagmodul/frontend/config/WebSocketConfig.kt
+++ src/main/kotlin/no/nav/eessi/fagmodul/frontend/config/WebSocketConfig.kt
@@ -1,13 +1,17 @@
 package no.nav.eessi.fagmodul.frontend.config
 
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
+private val logger = LoggerFactory.getLogger(WebSocketMessageBrokerConfigurer::class.java)
+
 @Configuration
 @EnableWebSocketMessageBroker
+
 class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     @Value("\${cors.allowed}")
@@ -15,5 +19,6 @@ class WebSocketConfig : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/websocket").setAllowedOrigins(allowed).withSockJS()
+        logger.info("Added STOMP endpoint /websocket, for ${allowed} access only")
     }
 }
