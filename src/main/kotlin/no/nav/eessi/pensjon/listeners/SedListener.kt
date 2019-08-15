7ff473fe0366e29a4dbdf9d1b29c2d9feeef72eb
erlend-axelsson <erlend.axelsson@gmail.com>
2019-08-15 12:11:01 +0200

    Fix remove dependencies to old STOMP config

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
@@ -1,11 +1,8 @@
 package no.nav.eessi.pensjon.listeners
 
-import no.nav.eessi.pensjon.utils.mapAnyToJson
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Description
 import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Component
 import java.util.concurrent.CountDownLatch
 
@@ -16,22 +13,10 @@ class SedListener {
     private val logger = LoggerFactory.getLogger(SedListener::class.java)
     private val latch = CountDownLatch(1)
 
-    @Autowired
-    lateinit var brokerMessagingTemplate: SimpMessagingTemplate
-
     @KafkaListener(topics = ["\${kafka.sedSendt.topic}"], groupId = "\${kafka.sedSendt.groupid}")
     fun consumeSedSendt(hendelse: String) {
         logger.info("Innkommet sedSendt hendelse")
         logger.debug(hendelse)
-        try {
-            brokerMessagingTemplate.convertAndSend("/sed",
-                mapAnyToJson(mapOf("action" to "Sent", "payload" to hendelse))
-            )
-        } catch(ex: Exception){
-            logger.error(
-                "Noe gikk galt under behandling av SED-hendelse:\n $hendelse \n${ex.message}", ex)
-            throw RuntimeException(ex.message)
-        }
         latch.countDown()
     }
 
@@ -39,14 +24,5 @@ class SedListener {
     fun consumeSedMottatt(hendelse: String) {
         logger.info("Innkommet sedMottatt hendelse")
         logger.debug(hendelse)
-        try {
-            brokerMessagingTemplate.convertAndSend("/sed",
-                mapAnyToJson(mapOf("action" to "Received", "payload" to hendelse))
-            )
-        } catch(ex: Exception){
-            logger.error(
-                "Noe gikk galt under behandling av SED-hendelse:\n $hendelse \n${ex.message}", ex)
-            throw RuntimeException(ex.message)
-        }
     }
 }
