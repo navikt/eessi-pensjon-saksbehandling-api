a7efefb0de1b8d18ab986a9c5444ba02960c4de8
erlend-axelsson <erlend.axelsson@gmail.com>
2019-08-16 17:35:33 +0200

    Connect websocket and kafka with alertSubscribers method

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
@@ -1,5 +1,7 @@
 package no.nav.eessi.pensjon.listeners
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import no.nav.eessi.pensjon.config.SocketTextHandler
 import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Description
 import org.springframework.kafka.annotation.KafkaListener
@@ -10,19 +12,38 @@ import java.util.concurrent.CountDownLatch
 @Description("Listener on kafka messages to send websocket notifications")
 class SedListener {
 
+    private val mapper = ObjectMapper()
     private val logger = LoggerFactory.getLogger(SedListener::class.java)
     private val latch = CountDownLatch(1)
 
     @KafkaListener(topics = ["\${kafka.sedSendt.topic}"], groupId = "\${kafka.sedSendt.groupid}")
     fun consumeSedSendt(hendelse: String) {
-        logger.info("Innkommet sedSendt hendelse")
-        logger.debug(hendelse)
-        latch.countDown()
+        try {
+            logger.info("Innkommet sedSendt hendelse")
+            logger.debug(hendelse)
+            val jsonNode = mapper.readTree(hendelse)
+            if(jsonNode.has("navBruker") && jsonNode["navBruker"].isTextual){
+                SocketTextHandler().alertSubscribers(jsonNode["navBruker"].textValue())
+            }
+            latch.countDown()
+        } catch(exception: Exception){
+            logger.error("Error when handling outgoing sedSendt event", exception)
+            throw exception
+        }
     }
 
     @KafkaListener(topics = ["\${kafka.sedMottatt.topic}"], groupId = "\${kafka.sedMottatt.groupid}")
     fun consumeSedMottatt(hendelse: String) {
+        try {
         logger.info("Innkommet sedMottatt hendelse")
         logger.debug(hendelse)
+            val jsonNode = mapper.readTree(hendelse)
+            if(jsonNode.has("navBruker") && jsonNode["navBruker"].isTextual){
+                SocketTextHandler().alertSubscribers(jsonNode["navBruker"].textValue())
+            }
+        } catch(exception: Exception){
+            logger.error("Error when handling incoming sedMottatt event", exception)
+            throw exception
+        }
     }
 }
