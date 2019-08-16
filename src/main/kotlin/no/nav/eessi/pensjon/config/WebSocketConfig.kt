6c686aed67e0157f3109d6cd7f68efd70e39342d
erlend-axelsson <erlend.axelsson@gmail.com>
2019-08-16 13:22:59 +0200

    Attempt to use oidcRequestContextHolder for websocket handshake

--- src/main/kotlin/no/nav/eessi/pensjon/config/WebSocketConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/WebSocketConfig.kt
@@ -1,5 +1,7 @@
 package no.nav.eessi.pensjon.config
 
+import no.nav.eessi.pensjon.interceptor.WebSocketHandShakeInterceptor
+import no.nav.security.oidc.context.OIDCRequestContextHolder
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
@@ -9,13 +11,13 @@ private val logger = LoggerFactory.getLogger(WebSocketConfigurer::class.java)
 
 @Configuration
 @EnableWebSocket
-class WebsocketConfiguration : WebSocketConfigurer {
+class WebsocketConfiguration(private val oidcRequestContextHolder: OIDCRequestContextHolder) : WebSocketConfigurer {
 
     @Value("\${cors.allowed}")
     lateinit var allowed: String
 
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry.addHandler(SocketTextHandler(), "/bucUpdate").setAllowedOrigins("*")
+        registry.addHandler(SocketTextHandler(), "/bucUpdate").setAllowedOrigins("*").addInterceptors(WebSocketHandShakeInterceptor(oidcRequestContextHolder))
         logger.info("Added websocket endpoint /bucUpdate, for ${allowed} access only")
     }
 }
