45a72a5c149a1ffd34d1f5ef3a02adbd92b8e318
Mo Amini <mo.amini@nav.no>
2019-09-16 12:15:47 +0200

    fjernet logging som spammet for mye

--- src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/listeners/SedListener.kt
@@ -6,23 +6,21 @@ import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Description
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
-import java.util.concurrent.CountDownLatch
 
 @Component
 @Description("Listener on kafka messages to send websocket notifications")
 class SedListener (private val socketTextHandler: SocketTextHandler) {
 
     private val logger = LoggerFactory.getLogger(SedListener::class.java)
-    private val latch = CountDownLatch(1)
 
     @KafkaListener(topics = ["\${kafka.sedSendt.topic}"], groupId = "\${kafka.sedSendt.groupid}")
     fun consumeSedSendt(hendelse: String) {
         try {
-            logger.info("Innkommet sedSendt hendelse")
-            logger.debug(hendelse)
             val sedHendelse = SedHendelseModel.fromJson(hendelse)
-            socketTextHandler.alertSubscribers(sedHendelse.rinaSakId, sedHendelse.navBruker)
-            latch.countDown()
+            if(sedHendelse.sektorKode == "P") {
+                logger.info("Innkommet sedSendt hendelse")
+                socketTextHandler.alertSubscribers(sedHendelse.rinaSakId, sedHendelse.navBruker)
+            }
         } catch (jsonParseException: JsonParseException) {
             logger.error("Error when parsing outgoing sedSendt Json", jsonParseException)
             throw jsonParseException
@@ -35,10 +33,11 @@ class SedListener (private val socketTextHandler: SocketTextHandler) {
     @KafkaListener(topics = ["\${kafka.sedMottatt.topic}"], groupId = "\${kafka.sedMottatt.groupid}")
     fun consumeSedMottatt(hendelse: String) {
         try {
-            logger.info("Innkommet sedMottatt hendelse")
-            logger.debug(hendelse)
             val sedHendelse = SedHendelseModel.fromJson(hendelse)
-            socketTextHandler.alertSubscribers(sedHendelse.rinaSakId, sedHendelse.navBruker)
+            if(sedHendelse.sektorKode == "P") {
+                logger.info("Innkommet sedMottatt hendelse")
+                socketTextHandler.alertSubscribers(sedHendelse.rinaSakId, sedHendelse.navBruker)
+            }
         } catch (jsonParseException: JsonParseException) {
             logger.error("Error when parsing outgoing sedMottatt Json", jsonParseException)
             throw jsonParseException
