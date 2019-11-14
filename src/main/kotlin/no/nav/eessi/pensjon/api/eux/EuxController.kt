b3cbd244bea682a74ea8343f0fef82ad7d6944a9
Mo Amini <mo.amini@nav.no>
2019-11-14 09:56:51 +0100

    ResponseEntity med korrekt httpstatus returneres

--- src/main/kotlin/no/nav/eessi/pensjon/api/eux/EuxController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/api/eux/EuxController.kt
@@ -2,14 +2,17 @@ package no.nav.eessi.pensjon.api.eux
 
 import no.nav.eessi.pensjon.services.eux.EuxService
 import no.nav.eessi.pensjon.services.fagmodul.NavRegistreOppslagService
+import no.nav.eessi.pensjon.utils.errorBody
 import no.nav.security.oidc.api.Protected
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpStatusCodeException
 
 
 @Protected
@@ -30,7 +33,13 @@ class EuxController(private val euxService: EuxService, private val navRegistreS
     @GetMapping("/institutions/{buctype}/{countrycode}")
     fun getInstitutionsWithCountry(@PathVariable(value = "buctype", required = true) bucType: String,
             @PathVariable(value = "countrycode", required = false) landkode: String = ""): ResponseEntity<String> {
-        return euxService.getInstitusjoner(bucType, landkode)
+        return try {
+            euxService.getInstitusjoner(bucType, landkode)
+        } catch (sce: HttpStatusCodeException) {
+            ResponseEntity.status(sce.statusCode).body(errorBody(sce.responseBodyAsString))
+        } catch (ex: Exception) {
+            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorBody(ex.message))
+        }
     }
 
     @GetMapping("/countrycode")
