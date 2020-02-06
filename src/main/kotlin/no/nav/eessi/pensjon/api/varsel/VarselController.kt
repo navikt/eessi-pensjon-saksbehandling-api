060b6846d8f5a38d3e08ead93e7e4dfac86e0110
moamini <mo.amini@nav.no>
2020-02-06 09:55:07 +0100

    Fjernes varsel funksjonalitet

--- src/main/kotlin/no/nav/eessi/pensjon/api/varsel/VarselController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/api/varsel/VarselController.kt
@@ -1,41 +1,22 @@
 package no.nav.eessi.pensjon.api.varsel
 
-import no.nav.eessi.pensjon.services.aktoerregister.AktoerregisterService
-import no.nav.eessi.pensjon.services.fagmodul.PersonInformasjonException
-import no.nav.eessi.pensjon.services.varsel.VarselService
-import no.nav.eessi.pensjon.services.varsel.VarselServiceException
 import no.nav.eessi.pensjon.utils.errorBody
-import no.nav.eessi.pensjon.utils.successBody
 import no.nav.security.oidc.api.Protected
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
-private val logger = LoggerFactory.getLogger(VarselController::class.java)
-
 @Protected
 @RestController
 @RequestMapping("/api/varsel")
-class VarselController(val varselService: VarselService,
-                       val aktoerregisterService: AktoerregisterService) {
+class VarselController {
 
     @PostMapping("/{saksId}/{aktoerId}")
     fun sendVarsel(@PathVariable(required = true) aktoerId: String,
                    @PathVariable(required = true) saksId: String): ResponseEntity<String> {
-        try {
-            val fnr = aktoerregisterService.hentGjeldendeNorskIdentForAktorId(aktoerId)
-            varselService.sendVarsel(fnr, saksId, "EessiPenVarsleBrukerUfore")
-        } catch( pie: PersonInformasjonException) {
-            val uuid = UUID.randomUUID().toString()
-            logger.error(pie.message + ": " + uuid)
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorBody(pie.message!!, uuid))
-        } catch( vse: VarselServiceException) {
-            val uuid = UUID.randomUUID().toString()
-            logger.error(vse.message + ": " + uuid)
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorBody(vse.message!!, uuid))
-        }
-        return ResponseEntity.ok().body(successBody())
+        val uuid = UUID.randomUUID().toString()
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(errorBody("Varsel funksjonaliteten er skrudd av, kontakt EESSI Pensjon for spørsmål", uuid))
     }
 }
