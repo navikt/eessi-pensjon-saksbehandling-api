e5a06a8190406bd2b56dce03fb52f1eabb4c31e2
Nuno Cardoso <Nuno.Cardoso@nav.no>
2020-03-09 13:42:47 +0100

    Feature toggling

--- src/test/kotlin/no/nav/eessi/pensjon/api/userinfo/UserInfoControllerTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/api/userinfo/UserInfoControllerTest.kt
@@ -1,5 +1,8 @@
 package no.nav.eessi.pensjon.api.userinfo
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.whenever
 import no.nav.eessi.pensjon.services.storage.S3StorageBaseTest
 import no.nav.eessi.pensjon.utils.mapAnyToJson
 import no.nav.eessi.pensjon.utils.mapJsonToAny
@@ -19,31 +22,73 @@ class UserInfoControllerTest : S3StorageBaseTest() {
     fun mockSetup() {
         val mockOidcContextolder = generateMockContextHolder()
         userInfoController = Mockito.spy(UserInfoController(mockOidcContextolder,whitelistService))
+
+
     }
 
-    @Disabled
-    @Test fun `Calling UserInfoController|getUserInfo returns OK response`() {
+    @Test fun `Calling UserInfoController|getUserInfo in Q2 returns OK response`() {
+        userInfoController.fasitEnvironmentName = "q2"
+        doReturn(true).whenever(userInfoController).checkWhitelist()
+
         val usr =  UserInfoResponse(subject ="12345678910",
-                role ="BRUKER",
-                allowed = true,
-            expirationTime = 1531157178000
+            role ="BRUKER",
+            allowed = true,
+            expirationTime = 1531157178000,
+            features = mapOf("P5000_VISIBLE" to true)
         )
         assertEquals(ResponseEntity.ok().body(mapAnyToJson(usr)), userInfoController.getUserInfo())
     }
 
-    @Disabled
-    @Test fun `Calling UserInfoController|getUserInfo saksbehandler returns OK response`() {
-        val userInfoController2 = Mockito.spy(UserInfoController(generateMockSaksbehContextHolder(),whitelistService))
+    @Test fun `Calling UserInfoController|getUserInfo in P returns OK response`() {
+        userInfoController.fasitEnvironmentName = "p"
+        doReturn(true).whenever(userInfoController).checkWhitelist()
+        val usr =  UserInfoResponse(subject ="12345678910",
+            role ="BRUKER",
+            allowed = true,
+            expirationTime = 1531157178000,
+            features = mapOf("P5000_VISIBLE" to false)
+        )
+        assertEquals(ResponseEntity.ok().body(mapAnyToJson(usr)), userInfoController.getUserInfo())
+    }
 
+    @Test fun `Calling UserInfoController|getUserInfo saksbehandler in Q2 returns OK response`() {
+        val userInfoController2 = Mockito.spy(UserInfoController(generateMockSaksbehContextHolder(),whitelistService))
+        userInfoController2.fasitEnvironmentName = "q2"
+        doReturn(true).whenever(userInfoController2).checkWhitelist()
         val usr =  UserInfoResponse(subject ="A123456",
             role ="SAKSBEHANDLER",
             allowed = true,
-            expirationTime = 1531157178000
+            expirationTime = 1531157178000,
+            features = mapOf("P5000_VISIBLE" to true)
         )
         assertEquals(ResponseEntity.ok().body(mapAnyToJson(usr)), userInfoController2.getUserInfo())
-
     }
 
+    @Test fun `Calling UserInfoController|getUserInfo saksbehandler in P returns OK response`() {
+        val userInfoController2 = Mockito.spy(UserInfoController(generateMockSaksbehContextHolder(),whitelistService))
+        userInfoController2.fasitEnvironmentName = "p"
+        doReturn(true).whenever(userInfoController2).checkWhitelist()
+        val usr =  UserInfoResponse(subject ="A123456",
+            role ="SAKSBEHANDLER",
+            allowed = true,
+            expirationTime = 1531157178000,
+            features = mapOf("P5000_VISIBLE" to false)
+        )
+        assertEquals(ResponseEntity.ok().body(mapAnyToJson(usr)), userInfoController2.getUserInfo())
+    }
+
+    @Test fun `Calling UserInfoController|getUserInfo saksbehandler in P with invalid person does not return features`() {
+        val userInfoController2 = Mockito.spy(UserInfoController(generateMockSaksbehContextHolder(),whitelistService))
+        userInfoController2.fasitEnvironmentName = "p"
+        doReturn(false).whenever(userInfoController2).checkWhitelist()
+        val usr =  UserInfoResponse(subject ="A123456",
+            role ="SAKSBEHANDLER",
+            allowed = false,
+            expirationTime = 1531157178000,
+            features = mapOf()
+        )
+        assertEquals(ResponseEntity.ok().body(mapAnyToJson(usr)), userInfoController2.getUserInfo())
+    }
     @Disabled
     @Test fun `Calling UserInfoController|getRole`() {
         assertEquals("BRUKER", getRole("12345678910"))
@@ -56,7 +101,8 @@ class UserInfoControllerTest : S3StorageBaseTest() {
         val usr =  UserInfoResponse(subject ="12345678910",
             role ="BRUKER",
             allowed = true,
-            expirationTime = 1531157178000
+            expirationTime = 1531157178000,
+            features = mapOf()
         )
         val result = userInfoController.getUserInfo()
         assertEquals(ResponseEntity.ok().body(mapAnyToJson(usr)), result)
