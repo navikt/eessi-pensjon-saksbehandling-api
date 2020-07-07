8308dd439b6db3ff948de7d1f77bf96143dd86c4
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2020-07-07 15:24:08 +0200

    Remove use of deprecated AktoerregisterService-methods

--- src/main/kotlin/no/nav/eessi/pensjon/api/person/NavRegistreOppslagController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/api/person/NavRegistreOppslagController.kt
@@ -1,6 +1,11 @@
 package no.nav.eessi.pensjon.api.person
 
+import no.nav.eessi.pensjon.personoppslag.aktoerregister.AktoerregisterIkkeFunnetException
 import no.nav.eessi.pensjon.personoppslag.aktoerregister.AktoerregisterService
+import no.nav.eessi.pensjon.personoppslag.aktoerregister.IdentGruppe
+import no.nav.eessi.pensjon.personoppslag.aktoerregister.ManglerAktoerIdException
+import no.nav.eessi.pensjon.personoppslag.aktoerregister.NorskIdent
+import no.nav.eessi.pensjon.personoppslag.aktoerregister.Result
 import no.nav.eessi.pensjon.services.fagmodul.NavRegistreOppslagService
 import no.nav.eessi.pensjon.services.fagmodul.PersonInformasjonException
 import no.nav.eessi.pensjon.utils.errorBody
@@ -33,14 +38,29 @@ class NavRegistreOppslagController(val navRegistreOppslagService: NavRegistreOpp
     }
 
     @GetMapping(value = ["/personinfo/hentgjeldendeaktoerid/{fnr}"], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun hentGjeldendeAktoerIdForNorskident(@PathVariable(required = true) fnr: String): ResponseEntity<String> {
-        logger.info("Henter norskidenter")
-        return try{
-            ResponseEntity.ok().body(aktoerregisterService.hentGjeldendeAktorIdForNorskIdent(fnr))
-        } catch (sce: HttpStatusCodeException) {
-            return ResponseEntity.status(sce.statusCode).body(errorBody(sce.responseBodyAsString))
-        } catch (ex: Exception) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorBody(ex.message))
-        }
-    }
+    fun hentGjeldendeAktoerIdForNorskident(@PathVariable(required = true) fnr: String): ResponseEntity<String> =
+            if (fnr.isBlank()) {
+                logger.warn("Kall har blankt fnr")
+                ResponseEntity.badRequest().body("blankt fnr")
+            } else {
+                when (val result = aktoerregisterService.hentGjeldendeIdentFraGruppe(IdentGruppe.AktoerId, NorskIdent(fnr))) {
+                    is Result.Found -> {
+                        logger.info("Returnerer gjeldende aktoerId for fnr")
+                        ResponseEntity.ok().body(result.value.id)
+                    }
+                    is Result.NotFound -> {
+                        logger.info("Fant ikke aktoerId for fnr")
+                        ResponseEntity.notFound().build<String>()
+                    }
+                    is Result.Failure -> {
+                        logger.error("Feil ved henting av AktoerId med ${result.cause} cause: ${result.cause.cause}", result.cause)
+                        if (result.cause.cause != null && result.cause.cause is HttpStatusCodeException) {
+                            val rootCause = result.cause.cause as HttpStatusCodeException
+                            ResponseEntity.status(rootCause.statusCode).body(rootCause.responseBodyAsString)
+                        } else {
+                            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result.cause.message)
+                        }
+                    }
+                }
+            }
 }
