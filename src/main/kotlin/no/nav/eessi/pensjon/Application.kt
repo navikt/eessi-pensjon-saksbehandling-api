ef6c25e80a2ac539abc9b74f235e454831d54dd3
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2022-03-17 14:08:13 +0100

    klargjøre for api i gcp.. part 2
    Co-authored-by: Daniel Skarpås <dskarpas@gmail.com>
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/Application.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/Application.kt
@@ -6,6 +6,8 @@ import org.springframework.boot.runApplication
 
 @EnableJwtTokenValidation(ignore = ["org.springframework", "springfox.documentation", "org.pac4j.springframework.web.CallbackController", "no.nav.eessi.pensjon.health.DiagnosticsController"])
 @SpringBootApplication
+@EnableOAuth2Client(cacheEnabled = true)
+@EnableCaching
 class Application
 
 fun main(args: Array<String>) {
