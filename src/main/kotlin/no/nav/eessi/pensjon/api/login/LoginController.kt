8baf0deb96455fcdaa9fd995d5eed28f9f709349
mariampervez <mariam.pervez@nav.no>
2022-03-30 10:14:48 +0200

    Legger til logincallback endpoint
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>
    Co-authored-by: Nuno Cardoso <nuno.cardoso@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/api/login/LoginController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/api/login/LoginController.kt
@@ -4,9 +4,12 @@ import no.nav.security.token.support.core.api.Unprotected
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import java.net.URLEncoder
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
@@ -22,21 +25,63 @@ class LoginController {
     lateinit var navDomain: String
 
 
+//    @Unprotected
+//    @GetMapping("/login")
+//    fun login(httpServletRequest: HttpServletRequest,
+//              httpServletResponse: HttpServletResponse,
+//              @RequestParam("redirect", required = true) redirectTo: String) {
+//
+//        httpServletRequest.headerNames
+////        @RequestParam("context", required = false) context: String?
+////        val encodedContext = URLEncoder.encode(context, "UTF-8")
+//
+//        val apppath = "$appName.$navDomain"
+//        val redir = "https://$apppath/oauth2/login?redirect=$redirectTo"
+//
+////        https://pensjon-utland-q2.nais.preprod.local?aktoerId=2953297351855&sakId=22955439&kravId=42501017&vedtakId=42791092
+//
+//        logger.debug("Redirecter til: $redir")
+//
+//        httpServletResponse.sendRedirect(redir)
+//
+//
+//    }
+
+//    @Unprotected
+//    @GetMapping("/callback")
+//    fun callback(httpServletRequest: HttpServletRequest,
+//                 httpServletResponse: HttpServletResponse,
+//                 @RequestParam("redirect", required = true) redirectTo: String) {
+//
+//        val callbackurl = "https://pensjon-utland-q2.nais.preprod.local?$redirectTo"
+//        httpServletResponse.sendRedirect(callbackurl)
+//
+//
+//    }
+
     @Unprotected
     @GetMapping("/login")
     fun login(httpServletRequest: HttpServletRequest,
               httpServletResponse: HttpServletResponse,
-              @RequestParam("redirect", required = true) redirectTo: String) {
+              @RequestParam("redirect") redirectTo: String,
+              @RequestParam("context", required = false) context: String) {
 
-//        @RequestParam("context", required = false) context: String?
-//        val encodedContext = URLEncoder.encode(context, "UTF-8")
+     //        https://pensjon-utland-q2.nais.preprod.local?aktoerId=2953297351855&sakId=22955439&kravId=42501017&vedtakId=42791092
 
-        val apppath = "$appName.$navDomain"
-        val redir = "https://pensjon-utland-q2.nais.preprod.local/_/s"
 
-        logger.debug("Redirecter til: $redir")
+        val callbackUrl = "https://${appName}.${navDomain}/logincallback?redirect=" + redirectTo + context
+        val redirectUrl = "https://${appName}.${navDomain}/oauth2/login?redirect=" + URLEncoder.encode(callbackUrl, "UTF-8")
 
-        httpServletResponse.sendRedirect(redir)
+        logger.debug("Redirecting to login: $redirectUrl")
+        httpServletResponse.sendRedirect(redirectUrl)
+    }
+
+    @Unprotected
+    @GetMapping("/logincallback")
+    fun openamlogin(httpServletResponse: HttpServletResponse, @RequestParam("redirect") redirect: String) {
+        logger.debug("Redirecting back to frontend: $redirect")
+        httpServletResponse.setHeader(HttpHeaders.LOCATION, "$redirect")
+        httpServletResponse.status = HttpStatus.FOUND.value()
 
     }
 
