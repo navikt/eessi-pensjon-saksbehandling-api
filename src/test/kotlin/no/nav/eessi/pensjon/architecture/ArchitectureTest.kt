a4ad09569e5d76e95a2c558108f61a991aadcb17
Daniel Skarpås <dskarpas@gmail.com>
2022-07-15 13:48:09 +0200

    t legger tilbake disablet archtest med rettelser
    - sjekker kun interne klasser, ikke biblioteker
    - legger til archtest.prop for å ikke feile på "empty sjekker"

--- src/test/kotlin/no/nav/eessi/pensjon/architecture/ArchitectureTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/architecture/ArchitectureTest.kt
@@ -8,10 +8,8 @@ import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices
 import no.nav.eessi.pensjon.Application
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-@Disabled
 class ArchitectureTest {
 
     companion object {
@@ -26,9 +24,12 @@ class ArchitectureTest {
         @BeforeAll
         @JvmStatic
         fun `extract classes`() {
-            classesToAnalyze = ClassFileImporter().withImportOption(ImportOption.DoNotIncludeTests()).importPackages(root)
-            assertTrue(classesToAnalyze.size > 100, "Sanity check on no. of classes to analyze")
-            assertTrue(classesToAnalyze.size < 800, "Sanity check on no. of classes to analyze")
+            classesToAnalyze = ClassFileImporter()
+                .withImportOption(ImportOption.DoNotIncludeTests())
+                .withImportOption(ImportOption.DoNotIncludeJars())
+                .importPackages(root)
+            assertTrue(classesToAnalyze.size > 10, "Sanity check on no. of classes to analyze")
+            assertTrue(classesToAnalyze.size < 300, "Sanity check on no. of classes to analyze")
         }
     }
 
@@ -43,27 +44,19 @@ class ArchitectureTest {
         Architectures.layeredArchitecture()
                 .layer("App").definedBy("$root")
                 .layer("API").definedBy("$root.api..", "$root.personoppslag..")
-                .layer("Eux").definedBy("$root.eux..")
-                .layer("Websockets").definedBy("$root.websocket..")
                 .layer("Listeners").definedBy("$root.listeners..")
                 .layer("Health").definedBy("$root.health..")
-                .layer("Metrics").definedBy("$root.metrics..")
                 .layer("Services").definedBy("$root.services..", "$root.personoppslag..")
                 .layer("Config").definedBy("$root.config..")
-                .layer("Security").definedBy("$root.security..")
                 .layer("Interceptor").definedBy("$root.interceptor..")
-                .layer("Logging").definedBy("$root.logging..")
                 .layer("Utils").definedBy("$root.utils..")
                 .whereLayer("App").mayNotBeAccessedByAnyLayer()
                 .whereLayer("API").mayNotBeAccessedByAnyLayer()
                 .whereLayer("Health").mayNotBeAccessedByAnyLayer()
                 .whereLayer("Listeners").mayNotBeAccessedByAnyLayer()
-                .whereLayer("Websockets").mayOnlyBeAccessedByLayers("Listeners")
                 .whereLayer("Services").mayOnlyBeAccessedByLayers("API", "Interceptor")
                 .whereLayer("Config").mayOnlyBeAccessedByLayers("API")
-                .whereLayer("Security").mayOnlyBeAccessedByLayers("Services", "Config", "Eux")
-                .whereLayer("Interceptor").mayOnlyBeAccessedByLayers("Config", "Logging")
-                .whereLayer("Logging").mayOnlyBeAccessedByLayers("API", "Config", "Interceptor", "Security", "Eux")
+
                 //Verify rules
                 .check(classesToAnalyze)
     }
