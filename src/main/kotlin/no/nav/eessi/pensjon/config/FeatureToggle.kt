9f5ce867de0d5b68c9c18154dc646fadc69b8b93
Daniel Skarpås <dskarpas@gmail.com>
2022-09-29 10:27:59 +0200

    r fjerner ubrukt featuretoggle og ident som ikke er i bruk

--- src/main/kotlin/no/nav/eessi/pensjon/config/FeatureToggle.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/FeatureToggle.kt
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component
 class FeatureToggle {
 
     private val listeFagfolkEllerSaksbehandlere = listOf(
-        "S128848",
         "K105134",
         "B101331"
     )
@@ -24,24 +23,18 @@ class FeatureToggle {
         return environmentName.contains("p", true)
     }
 
-    fun getUIFeatures(fnr: String): Map<String, Boolean> {
+    fun getUIFeatures(ident: String): Map<String, Boolean> {
         return mapOf(
-            FeatureName.P5000_SUMMER_VISIBLE.name to true,
-            FeatureName.P5000_UPDATES_VISIBLE.name to featureToggleP5000Updates(fnr),
-            FeatureName.X010_X009_VISIBLE.name to true,
-            FeatureName.ADMIN_NOTIFICATION_MESSAGE.name to featureToggleP5000Updates(fnr)
+            FeatureName.P5000_UPDATES_VISIBLE.name to featureToggleP5000Updates(ident),
+            FeatureName.ADMIN_NOTIFICATION_MESSAGE.name to featureToggleP5000Updates(ident)
         )
     }
 
-    fun featureToggleP5000Updates(fnr: String) : Boolean = (isProductionEnv() && fnr.uppercase() in listeFagfolkEllerSaksbehandlere) || !isProductionEnv()
+    fun featureToggleP5000Updates(ident: String) : Boolean = (isProductionEnv() && ident.uppercase() in listeFagfolkEllerSaksbehandlere) || !isProductionEnv()
 
 }
 
 enum class FeatureName {
-    // P5000 tables visibility
-    P5000_SUMMER_VISIBLE,
-    // X010/X009 visibility
-    X010_X009_VISIBLE,
     // New P5000 features visibility
     P5000_UPDATES_VISIBLE,
     // Administrate the notification message that shows up when EP page loads
