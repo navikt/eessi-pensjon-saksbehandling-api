e8659e058118202ae6bf0d8a05ab41c6596c62a5
Daniel Skarpås <dskarpas@gmail.com>
2022-12-30 13:33:57 +0100

    t - Flytter json tester til ep-eux

--- src/test/kotlin/no/nav/eessi/pensjon/utils/UtilsTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/utils/UtilsTest.kt
@@ -1,69 +1,10 @@
 package no.nav.eessi.pensjon.utils
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.jetbrains.annotations.TestOnly
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 
 class UtilsTest {
-
-    @Test
-    fun `Gitt en feilmelding når kall til errorBody så generer gyldig json`() {
-        val feilmelding = "En feilmelding"
-        val body = errorBody(feilmelding)
-
-        assertNotNull(body)
-        assertNotEquals("", body)
-        assertTrue(isValidJson(body))
-    }
-
-    @Test
-    fun `Gitt en feilmelding med feilmeldingstekst når kall til errorBody så må body inneholde error og success false`() {
-        val feilmelding = "En feilmelding"
-        val body = errorBody(feilmelding)
-
-        val mapper = ObjectMapper()
-
-        val tree = mapper.readTree(body)
-        assertEquals("En feilmelding" , tree.get("error").textValue())
-        assertEquals(false , tree.get("success").booleanValue())
-    }
-
-    @Test
-    fun `Gitt en feilmelding med tom feilmeldingstekst når kall til errorBody så må body inneholde error og success false`() {
-        val feilmelding = ""
-        val body = errorBody(feilmelding)
-
-        val mapper = ObjectMapper()
-
-        val tree = mapper.readTree(body)
-        assertEquals("" , tree.get("error").textValue())
-        assertEquals(false , tree.get("success").booleanValue())
-    }
-
-    @Test
-    fun `Gitt et kall til successBody så generer gyldig json`() {
-        val body = successBody()
-
-        assertNotNull(body)
-        assertNotEquals("", body)
-        assertTrue(isValidJson(body))
-    }
-
-    @Test
-    fun `Gitt et kall til successBody så må body inneholde success true`() {
-        val body = successBody()
-
-        val mapper = ObjectMapper()
-
-        val tree = mapper.readTree(body)
-        assertEquals(true , tree.get("success").booleanValue())
-    }
-
     @Test
     fun `Gitt en gyldig path når maskerer så masker kun personIdentifikator`() {
         assertEquals("***********___PINFO", maskerPersonIdentifier("12345678910___PINFO"))
@@ -75,17 +16,6 @@ class UtilsTest {
                 "***********___PINFO,***********___PINFO",
                 maskerPersonIdentifier(listOf("12345678910___PINFO", "10987654321___PINFO")
         ))
-    }
-
-    @TestOnly
-    fun isValidJson(json: String): Boolean {
-        val mapper = ObjectMapper()
-        return try{
-            mapper.readTree(json)
-            true
-        } catch (ex: Exception) {
-            false
-        }
     }
 
     @Test
