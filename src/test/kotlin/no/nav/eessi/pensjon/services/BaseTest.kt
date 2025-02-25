b311e61568d5854fa6e034cc3ab71529d7b47313
Daniel Skarpås <dskarpas@gmail.com>
2025-02-25 22:55:29 +0100

    Revert "E - fjerning av eldre versjon av token support; no.nav.security:token-validation-test-support:2.0.5"
    This reverts commit dc0f0ad010223419ce49127ee5724189437b4d26.

--- src/test/kotlin/no/nav/eessi/pensjon/services/BaseTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/services/BaseTest.kt
@@ -3,16 +3,17 @@ package no.nav.eessi.pensjon.services
 
 import com.unboundid.ldap.listener.InMemoryDirectoryServer
 import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig
-import okio.use
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Import
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @Suppress("DEPRECATION")
 @ActiveProfiles("test")
+@Import(no.nav.security.token.support.test.spring.TokenGeneratorConfiguration::class)
 @DirtiesContext
 @ExtendWith(SpringExtension::class)
 open class BaseTest {
@@ -35,6 +36,8 @@ open class BaseTest {
             server.connection.use { connection ->
                 System.setProperty("ldapServerPort", connection.connectedPort.toString())
             }
+
         }
     }
+
 }
