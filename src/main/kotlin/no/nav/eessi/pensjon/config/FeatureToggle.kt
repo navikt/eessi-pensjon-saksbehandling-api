c6c895cfc599af7ad4891316ad12377830ef7ee5
mariampervez <mariam.pervez@nav.no>
2025-05-23 13:21:40 +0200

    f - Fjerner alle andre enn Eddy og Anund fra listeFagfolkEllerSaksbehandlere listen

--- src/main/kotlin/no/nav/eessi/pensjon/config/FeatureToggle.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/FeatureToggle.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 @Component
 class FeatureToggle {
 
-    private val listeFagfolkEllerSaksbehandlere = listOf("K105134", "B101331", "R107597", "H129072", "S165969", "M159200", "R107620")
+    private val listeFagfolkEllerSaksbehandlere = listOf("K105134", "B101331")
 
     @Value("\${ENV}")
     private lateinit var environmentName: String
