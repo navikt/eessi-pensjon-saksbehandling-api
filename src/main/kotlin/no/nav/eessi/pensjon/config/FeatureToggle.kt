193a558e7b7d458734cdd7cefda67d73189668cb
Daniel Skarpås <dskarpas@gmail.com>
2025-09-10 12:36:00 +0200

    F - Nye testbrukere H103790, N128870, og K137167

--- src/main/kotlin/no/nav/eessi/pensjon/config/FeatureToggle.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/FeatureToggle.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 @Component
 class FeatureToggle {
 
-    private val listeOverTestere = listOf("B101331", "K105134", "L137579", "T120898", "K137167", "S137110", "H145594", "E153764", "B170313", "S165198", "O107147", "R107597", "R170375")
+    private val listeOverTestere = listOf("B101331", "K105134", "L137579", "T120898", "K137167", "S137110", "H145594", "E153764", "B170313", "S165198", "O107147", "R107597", "R170375", "N128870", "H103790", "K137167")
     private val listeOverAdmins = listOf("B101331", "K105134")
 
     @Value("\${ENV}")
