05eb014af624b8f7af21c6ad59076fa3450b33fd
Daniel Skarpås <dskarpas@gmail.com>
2025-09-17 13:01:17 +0200

    T - Legger til Unleash for test

--- src/test/kotlin/no/nav/eessi/pensjon/ApplicationStartupTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/ApplicationStartupTest.kt
@@ -2,6 +2,7 @@ package no.nav.eessi.pensjon
 
 import com.ninjasquad.springmockk.MockkBean
 import com.ninjasquad.springmockk.MockkBeans
+import io.getunleash.Unleash
 import no.nav.eessi.pensjon.config.ApiMvcConfig
 import no.nav.eessi.pensjon.config.UnleashConfigEessi
 import no.nav.eessi.pensjon.gcp.GcpStorageService
@@ -21,7 +22,9 @@ import org.springframework.boot.test.context.SpringBootTest
     MockkBean(classes = [ApiMvcConfig::class], relaxed = true),
     MockkBean(classes = [AuthInterceptor::class], relaxed = true),
     MockkBean(classes = [SedListener::class], relaxed = true),
-    MockkBean(classes = [UnleashConfigEessi::class], relaxed = true)
+    MockkBean(classes = [UnleashConfigEessi::class], relaxed = true),
+    MockkBean(classes = [Unleash::class], relaxed = true)
+
 )
 internal class ApplicationStartupTest {
 
