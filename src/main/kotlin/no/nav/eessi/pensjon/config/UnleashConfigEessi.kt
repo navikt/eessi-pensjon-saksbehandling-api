f53f10d778b19c2c43b1fd141731e55b9a58c1eb
Daniel Skarpås <dskarpas@gmail.com>
2025-09-19 08:50:24 +0200

    F - Fjerner profil for UnleashConfigEessi

--- src/main/kotlin/no/nav/eessi/pensjon/config/UnleashConfigEessi.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/UnleashConfigEessi.kt
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
 @Configuration
-@Profile("test")
 class UnleashConfigEessi(
     @param:Value("\${UNLEASH_URL}") private val unleashUrl: String,
     @param:Value("\${UNLEASH_APP_NAME}") private val appName: String,
