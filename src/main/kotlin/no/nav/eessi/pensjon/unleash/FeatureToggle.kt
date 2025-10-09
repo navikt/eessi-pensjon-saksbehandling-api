ef5a773add7cc54cf11901b8b8594c32a8731c38
Daniel Skarpås <dskarpas@gmail.com>
2025-10-09 10:23:39 +0200

    F - Bruker unleash for å hente alle tilgjengelige toggles

--- src/main/kotlin/no/nav/eessi/pensjon/unleash/FeatureToggle.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/unleash/FeatureToggle.kt
@@ -54,17 +54,7 @@ class FeatureToggle(val featureToggleService: FeatureToggleService){
         userList: List<String>
     ) : Boolean = (isProductionEnv() && ident.uppercase() in userList) || !isProductionEnv()
 
-    private fun allFeaturesEnabled() = mapOf(
-        FeatureName.TEST_USER.name to true,
-        FeatureName.ADMIN_USER.name to true,
-        FeatureName.P5000_UPDATES_VISIBLE.name to true
-    )
-
-    private fun allFeaturesDisabled() = mapOf(
-        FeatureName.TEST_USER.name to false,
-        FeatureName.ADMIN_USER.name to false,
-        FeatureName.P5000_UPDATES_VISIBLE.name to false
-    )
+    private fun allFeaturesEnabled() = featureToggleService.getAllFeaturesForProject().associate { it.name to it.enabled }
 
     private fun allFeaturesOldSchool(ident: String) = mapOf(
         FeatureName.ADMIN_USER.name to featureToggle(ident, listeOverAdmins),
