ca6bbca7f77939ea70501f99a97527222a114f02
Daniel Skarpås <dskarpas@gmail.com>
2025-10-13 13:24:17 +0200

    T - Legger inn generering av token sammen med claims for uthenting av bruker fra authenticator

--- src/test/kotlin/no/nav/eessi/pensjon/api/userinfo/UserInfoControllerTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/api/userinfo/UserInfoControllerTest.kt
@@ -1,13 +1,11 @@
 package no.nav.eessi.pensjon.api.userinfo
 
-import com.nimbusds.jwt.JWTClaimsSet
 import com.ninjasquad.springmockk.MockkBean
 import com.ninjasquad.springmockk.MockkBeans
 import com.ninjasquad.springmockk.SpykBean
 import com.ninjasquad.springmockk.SpykBeans
 import io.micrometer.core.instrument.MeterRegistry
 import io.mockk.every
-import io.mockk.spyk
 import no.nav.eessi.pensjon.api.storage.StorageController
 import no.nav.eessi.pensjon.gcp.GcpStorageService
 import no.nav.eessi.pensjon.models.BrukerInformasjon
@@ -18,11 +16,9 @@ import no.nav.eessi.pensjon.utils.mapAnyToJson
 import no.nav.eessi.pensjon.utils.mapJsonToAny
 import no.nav.eessi.pensjon.utils.toJson
 import no.nav.security.mock.oauth2.MockOAuth2Server
-import no.nav.security.token.support.core.jwt.JwtTokenClaims
 import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
@@ -35,7 +31,6 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.web.client.RestTemplate
-import java.util.*
 
 @ActiveProfiles(profiles = ["unsecured-webmvctest"])
 @EnableMockOAuth2Server
@@ -58,8 +53,6 @@ class UserInfoControllerTest {
 
     @Value("\${no.nav.security.jwt.issuer.aad.accepted_audience}")
     lateinit var audience: String
-
-    private val EXPIRATION_TIME = 1300819380L
 
     @Autowired
     private lateinit var mockMvc: MockMvc
@@ -98,29 +91,31 @@ class UserInfoControllerTest {
     }
 
     @Test fun `Calling UserInfoController getUserInfo returns OK response`() {
-        val brukerInfo = BrukerInformasjon(ident = "12345678910", medlemAv = listOf("0000-GA-Pensjon_Utland", "0000-ga-eessi-basis"))
+        val brukerInfo = BrukerInformasjon(
+            ident = "12345678910",
+            medlemAv = listOf("0000-GA-Pensjon_Utland", "0000-ga-eessi-basis")
+        )
+        val ident = "Z123456"
+        every { restTemplate.exchange(any<String>(), any(), any(), String::class.java) } returns ResponseEntity(
+            brukerInfo.toJson(), HttpStatus.OK
+        )
 
-        every { restTemplate.exchange(any<String>(), any(), any(), String::class.java) }returns ResponseEntity(brukerInfo.toJson(), HttpStatus.OK)
-            val token = mockOAuth2Server.issueToken("aad", "12345678910", audience).serialize()
+        val token = generateMockAuth2Token(ident)
 
         val response = mockMvc.perform(
-            get("/api/userinfo")
-                .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
-        )
-            .andExpect(status().isOk)
-            .andReturn().response
+            get("/api/userinfo").header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+        ).andExpect(status().isOk).andReturn().response
 
-        val usr =  UserInfoResponse(
-            subject ="Unknown",
-            role ="UNKNOWN",
-            expirationTime = EXPIRATION_TIME,
-            features = mapOf(
-                "P5000_SUMMER_VISIBLE" to true,
-                "P5000_UPDATES_VISIBLE" to true,
-            ),
-        )
         val expected = mapAnyToJson(
-            usr.copy(expirationTime = mapJsonToAny<UserInfoResponse>(response.contentAsString).expirationTime)
+            UserInfoResponse(
+                subject = ident,
+                role = "SAKSBEHANDLER",
+                expirationTime = mapJsonToAny<UserInfoResponse>(response.contentAsString).expirationTime,
+                features = mapOf(
+                    "P5000_SUMMER_VISIBLE" to true,
+                    "P5000_UPDATES_VISIBLE" to true
+                )
+            )
         )
         assertEquals(expected, response.contentAsString)
     }
@@ -131,42 +126,13 @@ class UserInfoControllerTest {
         assertEquals("UNKNOWN", getRole("ZZZ"))
     }
 
-    @Test fun CallingUserInfoController_getUserInfowithEXP() {
-        createMockedToken()
-        val usr =  UserInfoResponse(
-            subject ="12345678910",
-            role ="BRUKER",
-            expirationTime = EXPIRATION_TIME,
-            features = mapOf(
-                "P5000_SUMMER_VISIBLE" to true,
-                "P5000_UPDATES_VISIBLE" to true,
-                "X010_X009_VISIBLE" to true,
-                "ADMIN_NOTIFICATION_MESSAGE" to true
-            ),
-        )
-        val response = mockMvc.perform(
-            get("/api/userinfo")
-                .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
-        )
-            .andExpect(status().isOk)
-            .andReturn().response
-
-        val result = response.contentAsString
-        assertEquals(ResponseEntity.ok().body(mapAnyToJson(usr)), result)
-        val resultUserInfo = mapJsonToAny<UserInfoResponse>(result.body!!)
-        assertEquals(EXPIRATION_TIME, resultUserInfo.expirationTime)
-    }
-
-    private fun createMockedToken(subject: String = "12345678910") {
-        val claimsSet = JWTClaimsSet.Builder()
-            .subject(subject)
-            .expirationTime(Date(EXPIRATION_TIME))
-            .claim("http://example.com/is_root", true)
-            .build()
-        val twtToken = JwtTokenClaims(claimsSet)
-        spyk(twtToken)
-
-//        every { userInfoController.getSubjectFromToken() } returns subject
-//        every { userInfoController.getClaims() } returns twtToken
+    private fun generateMockAuth2Token(ident: String): String? {
+        val token = mockOAuth2Server.issueToken(
+            issuerId = "aad",
+            subject = "12345678910",
+            audience = audience,
+            claims = mapOf("NAVident" to ident)
+        ).serialize()
+        return token
     }
 }
